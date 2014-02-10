d3c455658f6d9fd7c255bf37672427deabb39089
cj <cjlazell@gmail.com>
2014-02-10 13:38:03 -0600

    fix to nav

--- lib/essential/presenters/base.rb
+++ lib/essential/presenters/base.rb
@@ -4,6 +4,13 @@ module Essential
       attr_reader :object, :template
 
       def initialize(object, template)
+        # Have to do this because Rails doesn't exist
+        # when Essential::Presenter::Base is first loaded
+        # idealy we should remove this.
+        Essential::Presenter::Base.class_eval do
+          include Rails.application.routes.url_helpers
+        end
+
         @object = object
         @template = template
       end
